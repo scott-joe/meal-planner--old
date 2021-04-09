56911778901b15b84a65e8a8a5ad1e6ee004298e
Scott Williams <scott.williams4@kroger.com>
2021-04-09 16:18:52 -0400

    Change to arrow functions

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
-const recipes = require("./recipes");
-const utils = require("./utils");
+const recipes = require('./recipes')
+const utils = require('./utils')
 
 function findNewRecipe(lastPlan) {
-  const number = utils.randomIntFromRange(recipes.length - 1);
-  const item = recipes.splice(number, 1)[0];
+	const number = utils.randomIntFromRange(recipes.length - 1)
+	const item = recipes.splice(number, 1)[0]
 
-  if (!lastPlan.includes(item.name)) {
-    return item;
-  } else {
-    return findNewRecipe(lastPlan);
-  }
+	if (!lastPlan.includes(item.name)) {
+		return item
+	} else {
+		return findNewRecipe(lastPlan)
+	}
 }
 
 function generateMealPlan(lastPlan) {
-  let count = 7;
-  let list = [];
+	let count = 7
+	let list = []
 
-  while (count > 0) {
-    list.push(findNewRecipe(lastPlan));
-    count = count - 1;
-  }
+	while (count > 0) {
+		list.push(findNewRecipe(lastPlan))
+		count = count - 1
+	}
 
-  return list;
+	return list
 }
 
-const lastPlan = utils.getLastPlan();
-const mealPlan = generateMealPlan(lastPlan);
-utils.savePlan(mealPlan);
+const lastPlan = utils.getLastPlan()
+const mealPlan = generateMealPlan(lastPlan)
+utils.savePlan(mealPlan)
 
-console.log(mealPlan.map((meal) => meal.name));
+console.log(mealPlan.map((meal) => meal.name))
--- utils.js
+++ utils.js
@@ -1,28 +1,28 @@
-const fs = require("fs");
+const fs = require('fs')
 
-const randomIntFromRange = function (max) {
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - 1));
-};
+const randomIntFromRange = (max) => {
+	max = Math.floor(max)
+	return Math.floor(Math.random() * (max - 1))
+}
 
-const getLastPlan = function () {
-  const lastPlan = [];
+const getLastPlan = () => {
+	const lastPlan = []
 
-  try {
-    const lastPlan = JSON.parse(fs.readFileSync("./lastPlan.json"));
-  } catch (error) {
-    console.log(error);
-  }
+	try {
+		const lastPlan = JSON.parse(fs.readFileSync('./lastPlan.json'))
+	} catch (error) {
+		console.log(error)
+	}
 
-  return lastPlan.map((value) => value.name, []);
-};
+	return lastPlan.map((value) => value.name, [])
+}
 
-const savePlan = function (plan) {
-  fs.writeFileSync("./lastPlan.json", JSON.stringify(plan));
-};
+const savePlan = (plan) => {
+	fs.writeFileSync('./lastPlan.json', JSON.stringify(plan))
+}
 
 module.exports = {
-  randomIntFromRange,
-  getLastPlan,
-  savePlan,
-};
+	randomIntFromRange,
+	getLastPlan,
+	savePlan,
+}
